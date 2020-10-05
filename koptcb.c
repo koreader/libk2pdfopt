24730a230500d130c26d7d16b9e9871f06448a82
Ez Diy <ezdiy@outlook.com>
2020-10-05 07:54:15 +0200

    add koptcb

---
+++ koptcb.c
@@ -0,0 +1,5 @@
+// called whenever k2pdfopt creates a pdf context
+// usually, we don't care, but may need to expose it as a callback to Lua one day
+
+void pdf_install_load_system_font_funcs(fz_context *ctx) {
+}
