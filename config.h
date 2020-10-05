866c3bb8cde152bc39b8ecbc77ad596b01274e8e
Ez Diy <ezdiy@outlook.com>
2020-10-05 16:14:58 +0200

    Add forgotten defines to config.h

--- config.h
+++ config.h
@@ -1,10 +1,11 @@
 #ifndef _K2OPT_CONFIG_H
 #define _K2OPT_CONFIG_H
 
-#define HAVE_TESSERACT_LIB
 #define HAVE_Z_LIB
+#define HAVE_PNG_LIB
 #define HAVE_JPEG_LIB
 #define HAVE_DJVU_LIB
+#define HAVE_MUPDF_LIB
 #define HAVE_LEPTONICA_LIB
 #define HAVE_TESSERACT_LIB
 
