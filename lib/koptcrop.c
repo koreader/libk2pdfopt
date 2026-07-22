9ab5acd0e3856d29743df659a8bc89c8b99e2e9b
Tachibana Shin <tachibshin@duck.com>
2026-07-22 14:38:40 +0700

    refactor: rename min function to static k2_min

--- lib/koptcrop.c
+++ lib/koptcrop.c
@@ -29,7 +29,7 @@
 #include "setting.h"
 #include "koptcrop.h"
 
-float min(float a, float b) {
+static float k2_min(float a, float b) {
     return (a < b) ? a : b;
 }
 
@@ -95,10 +95,10 @@ void k2pdfopt_crop_bmp(KOPTContext *kctx) {
 		margin = 0;
 
 	if (kctx->trim == 1) {
-		kctx->bbox.x0 = (float)region->c1 - min(margin, region->c1 - original_c1); // margin left
-		kctx->bbox.y0 = (float)region->r1 - min(margin, region->r1 - original_r1); // margin top;
-		kctx->bbox.x1 = (float)region->c2 + min(margin, original_c2 - region->c2); // margin right;
-		kctx->bbox.y1 = (float)region->r2 + min(margin, original_r2 - region->r2); // margin bottom;
+		kctx->bbox.x0 = (float)region->c1 - k2_min(margin, region->c1 - original_c1); // margin left
+		kctx->bbox.y0 = (float)region->r1 - k2_min(margin, region->r1 - original_r1); // margin top;
+		kctx->bbox.x1 = (float)region->c2 + k2_min(margin, original_c2 - region->c2); // margin right;
+		kctx->bbox.y1 = (float)region->r2 + k2_min(margin, original_r2 - region->r2); // margin bottom;
 	} else {
 		kctx->bbox.x0 = (float)region->c1 - margin;
 		kctx->bbox.y0 = (float)region->r1 - margin;
